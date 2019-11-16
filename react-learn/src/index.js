3a306f6fd28a7349ef36f5e31c94631b34fca6de
zhichengyuan <1798788388@qq.com>
2019-11-16 16:22:30 +0800

    学生组件

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -6,18 +6,5 @@ import MyClassComp from './MyClassComp';
 
 
 ReactDOM.render( <div>
-    <MyFuncComp number="3"/>
-    <MyFuncComp number={4}/>
-    <MyFuncComp number={5}/>
-    <MyFuncComp number={6}/>
-    <MyFuncComp number={7}/>
-
-    <MyClassComp />
-    <MyClassComp number={5} enable obj={{
-        name:'明天',
-        age:'20'
-    }}/>
-    <MyClassComp number={6} ui={<h2>这是我传递的属性</h2>}/>
-    <MyClassComp number={7}/>
-    <MyClassComp/>
+  
 </div> , document.getElementById('root'));
