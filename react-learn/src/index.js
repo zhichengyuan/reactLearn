5041a08e2376c849b4c25ac0e619199ca2c65131
zhichengyuan <1798788388@qq.com>
2019-11-20 10:33:39 +0800

    组件状态

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -6,20 +6,3 @@ var appkey = 'demo13_1545210570249';
 // var appkey = 'yuanzhicheng_1563606632795';
 var baseurl = 'http://open.duyiedu.com';
 
-
-//获取学生数据
-async function fetchAllStudents() {
-   var stus = await fetch(baseurl + '/api/student/findAll?appkey=' + appkey)
-        .then(res => res.json()).then(res => res.data)
-    console.log(stus);
-    return stus;
-}
-
-async function render() {
-    ReactDOM.render("正在加载中。。。。",document.getElementById('root'));
-    const stus = await fetchAllStudents();//获取学生数组
-    ReactDOM.render( <StudentList stus={stus}/> , document.getElementById('root'));
-}
-render()
-
-// ReactDOM.render( <Student {...s}/> , document.getElementById('root'));
