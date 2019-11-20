3fe93bfe1f139af81d3cb397da09c96711e80e93
zhichengyuan <1798788388@qq.com>
2019-11-20 10:36:21 +0800

    1

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from './components/StudentList';
 
 var appkey = 'demo13_1545210570249';
 // var appkey = 'yuanzhicheng_1563606632795';
