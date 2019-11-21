afdc80d0998ddab2261481dae55221662e2c7590
zhichengyuan <1798788388@qq.com>
2019-11-21 09:47:09 +0800

    组件状态

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Tick from './components/Tick'
 
-var appkey = 'demo13_1545210570249';
-// var appkey = 'yuanzhicheng_1563606632795';
-var baseurl = 'http://open.duyiedu.com';
+// var appkey = 'demo13_1545210570249';
+// // var appkey = 'yuanzhicheng_1563606632795';
+// var baseurl = 'http://open.duyiedu.com';
 
+ReactDOM.render( <Tick number={10}/> , document.getElementById('root'));
