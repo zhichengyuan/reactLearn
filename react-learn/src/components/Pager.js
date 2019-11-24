33cae52c348415a9ee0552b2da14f6e0b4cd25b3
zhichengyuan <1798788388@qq.com>
2019-11-24 17:56:39 +0800

    121

--- react-learn/src/components/Pager.js
+++ react-learn/src/components/Pager.js
@@ -12,9 +12,22 @@ import './Pager.css'
  */
 
 export default function Pager(props) {
+    const pageNumber = getPageNumber(props);
     return (
         <>
             <span className={props.current === 1 ? "item disabled" : 'item'}>首页</span>
+            <span className={props.current === 1 ? "item disabled" : 'item'}>上一页</span>
+            {/* 数字页码 */}
+            <span className={props.current === pageNumber ? "item disabled" : 'item'}>下一页</span>
+            <span>{props.current}</span>
+            <span>{pageNumber}</span>
         </>
     )
+}
+
+/**
+ * 计算总页数
+ */
+function getPageNumber(props) {
+    return Math.ceil(props.total / props.limit);
 }
