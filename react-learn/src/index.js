f7588550ca5187f5d264f211e4da1815a758b518
zhichengyuan <1798788388@qq.com>
2019-11-27 10:15:52 +0800

    传递元素内容

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import Comp from './Comp';
 
 
 
-    ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render((
+    <Comp html={<h1>顶顶顶顶</h1>}>
+        
+    </Comp>
+), document.getElementById('root'));
