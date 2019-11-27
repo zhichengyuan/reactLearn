799478720a480c5d404667dcafd7cd1d63f28cf3
zhichengyuan <1798788388@qq.com>
2019-11-27 21:04:42 +0800

    蒙层完成

--- react-learn/src/components/Test.js
+++ react-learn/src/components/Test.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react'
 import Modal from './common/Modal'
 
 export default class Test extends Component {
+    state = {
+        showModal:false
+    }
+
+    showModal = () =>{
+        this.setState({
+            showModal:true
+        })
+    }
+
+    hideModal = () => {
+        this.setState({
+            showModal:false
+        })
+    }
+
     render() {
         return (
             <div>
-                <Modal>
-                    <h2>发的发生的法定</h2>
-                </Modal>
+                {
+                    this.state.showModal ?
+                        (<Modal onClose={this.hideModal}>
+                            <h2>发的发生的法定</h2>
+                            <button onClick={this.hideModal}>关闭蒙层</button>
+                        </Modal>) : 
+                        null
+                }
+                <button onClick={this.showModal}>显示蒙层</button>
             </div>
         )
     }
--- react-learn/src/components/common/Modal/index.js
+++ react-learn/src/components/common/Modal/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './index.css'
 
-export default function Modal() {
+export default function Modal(props) {
+    var defaultProps = {//默认属性
+        bg:"rgba(0,0,0,.5)",
+    };
+    var datas = Object.assign({},defaultProps,props);
+
+    console.log(props);
     return (
-        <div>
+        <div onClick={e => {
+            if(e.target.className === 'modal') {
+                datas.onClose();
+            }
+        }} className="modal" style={{
+            background:datas.bg
+        }}>
+            <div className="modal-center">
+               {props.children}
+            </div>
         </div>
     )
 }
