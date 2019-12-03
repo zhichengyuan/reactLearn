5ef290c8469c70ee871e7000631fd8708fb06263
zhichengyuan <1798788388@qq.com>
2019-12-03 22:18:27 +0800

    hoc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 // import Test from './components/common/CheckBoxGroup/Test'
 // import Test from './components/common/RadioBoxGroup/Test'
 // import Test from './components/common/Select/Test'
-import Test from './components/Test'
+// import Test from './components/Test'
+import {A,B} from './components/Comps';
+import withLog from './HOC/withLog'
+import withLogin from './HOC/withLogin'
+let AComp = withLogin(withLog(A,'sdasdf'));
+let BComp = withLogin(withLog(B,'ewewew'));
 
 
 export default class App extends Component {
     
     render() {
-       
+        
         return (
             <div>
-                <Test/>
+                <AComp isLogin a={2}/>
+                <BComp isLogin b={3}/>
+                <button onClick={() => {
+                    this.setState({})
+                }}>点击</button>
             </div>
         )
     }
--- react-learn/src/HOC/withLog.js
+++ react-learn/src/HOC/withLog.js
@@ -9,15 +9,15 @@ export default function withLog(Comp) {
     return class LogWrapper extends React.Component{
 
         componentDidMount() {
-            console.log(`日志：组件${Comp.name}被创建了！${Date.now}`);
+            console.log(`日志：组件${Comp.name}被创建了！${Date.now()}`);
         }
 
         componentWillUnmount() {
-            console.log(`日志：组件${Comp.name}被销毁了！${Date.now}`)
+            console.log(`日志：组件${Comp.name}被销毁了！${Date.now()}`)
         }
 
         render() {
-            return <Comp />
+            return <Comp {...this.props}/>
         }
     }
 }
