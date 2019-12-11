56a1d8a2a37c8c97e39f2e3323102d318c2db4f3
zhichengyuan <1798788388@qq.com>
2019-12-11 10:23:59 +0800

    完成Portals

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
-import TaskContainer from "./components/TaskContainer"
+import ReactDOM from 'react-dom'
 
+
+function ChildA() {
+    return ReactDOM.createPortal(<div className="child-a" style={{marginTop:200}}>
+        <h1>ChildA</h1>
+    <ChildB/>
+</div>,document.querySelector('.modal')) 
+    
+}
+
+function ChildB() {
+    return <div className="child-b">
+        <h1>ChildB</h1>
+    </div>
+}
 
 export default class App extends Component {
     
@@ -8,8 +22,11 @@ export default class App extends Component {
     render() {
         
         return (
-            <div>
-               <TaskContainer />
+            <div className='app' onClick={(e) => {
+                console.log('被点击了',e.target);
+            }}>
+                <h1>app</h1>
+              <ChildA />
             </div>
         )
     }
