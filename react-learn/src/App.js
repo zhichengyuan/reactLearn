9812276b02cd552961049b338143d58adcc03059
zhichengyuan <1798788388@qq.com>
2019-12-19 21:43:27 +0800

    EffectHook

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,19 +1,33 @@
 import React, { useState,useEffect } from 'react'
 
+let n = 1 ;
+
+function func1() {
+    console.log('odd 副作用函数');
+    return () => {
+        console.log('odd 清理函数');
+    }
+}
+function func2() {
+    console.log('even 副作用函数');
+    return () => {
+        console.log('even 清理函数');
+    }
+}
+
 export default function App() {
-    const [n,setN] = useState(0);
-    useEffect(() => {
-        setTimeout(() => {
-            console.log(n);
-        },5000)
-    })
+    const [,forceUpdate] = useState({})
+  
+    useEffect(n%2 === 0 ? func2 : func1);
+    n++;
+
     return (
         <div>
-            <h1>{n}</h1>
+           
             
             <button onClick={() => {
-                setN(n+1);
-            }}>n+1</button>
+                forceUpdate({});
+            }}>强制刷新</button>
         </div>
     )
 }
