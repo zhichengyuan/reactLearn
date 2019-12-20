121603c0fbf3bcabccf5c730359ed9d0be436d90
zhichengyuan <1798788388@qq.com>
2019-12-20 17:03:30 +0800

    分页完成

--- react-learn/src/components/StudentContainer/index.js
+++ react-learn/src/components/StudentContainer/index.js
@@ -1,5 +1,6 @@
 import React,{ useState,useEffect } from 'react'
 import StudentList from '../StudentList'
+import Pager from '../common/Pager/Pager.js'
 import { getStudents } from '../../services/student'
 // getStudents().then(data => console.log(data));
 
@@ -9,21 +10,44 @@ import { getStudents } from '../../services/student'
 export default function StudentContainer() {
     const [students,setStudents] = useState([]);
     const [page,setPage] = useState(1);
-
+    const [limit,setLimit] = useState(10);
+    const [total,setTotal] = useState(0);
+    const [panelNumber,setPanelNumber] = useState(5);
+   
+    //当页吗和页容量发生变化时，将重新获取数据
     useEffect(() => {
         (async function () {
-            const resp = await getStudents(page,10);
+            const resp = await getStudents(page,limit);
             console.log(resp);
             setStudents(resp.findByPage);
+            setTotal(resp.cont)
         })();
-    },[page])//page发生变化时，需要重新获取数据
+    },[page,limit])//page发生变化时，需要重新获取数据
 
     return (
         <div>
             <StudentList stus={students}/>
-            <input type="number" value={page} onChange={e => {
-                setPage(parseInt(e.target.value))
-            }}/>
+            <Pager 
+                current={page} 
+                limit={limit} 
+                total={total} 
+                panelNumber={panelNumber}
+                onPageChange={newPage => {
+                    setPage(newPage);
+                }}    
+            />
+            <p>
+                每页显示的条数：
+                <input type="number" value={limit} onChange={e => {
+                    setLimit(parseInt(e.target.value));
+                }} />
+            </p>
+            <p>
+                最多显示的页码数：
+                <input type="number" value={panelNumber} onChange={e => {
+                    setPanelNumber(parseInt(e.target.value));
+                }} />
+            </p>
         </div>
     )
 }
