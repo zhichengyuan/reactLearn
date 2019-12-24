058603639e532b255fc762a587844dfa0a3b06b4
zhichengyuan <1798788388@qq.com>
2019-12-24 16:47:42 +0800

    2-12完成

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,40 +1,19 @@
-import React,{useRef,useImperativeHandle} from 'react'
-
-function Test(props,ref) {
-    useImperativeHandle(ref,() => {
-        //该函数第一次加载组件后调用
-        //如果使用依赖项，则第一次调用后，会进行缓存，只有依赖项发生变化时才会重新调用函数
-        //相当于给ref.current = 1;
-        return {
-            method() {
-                console.log('Test method called');
-            }
-        }
-    },[])
-    return <h1 ref={ref}>Test Component</h1>
-}
-const TestWrapper = React.forwardRef(Test)
-// class Test extends React.Component {
-//     method() {
-//         console.log('Test method called');
-//     }
-
-//     render() {
-//         return <h1>Test Component</h1>
-//     }
-// }
+import React,{useState,useEffect,useLayoutEffect,useRef} from 'react'
 
 export default function App() {
-    const testRef = useRef();
+    const [n, setN] = useState(0);
+    const h1Ref = useRef();
+    useLayoutEffect(() => {
+        h1Ref.current.innerText = Math.random().toFixed(2);
+    })
     return (
         <div>
-            <TestWrapper ref={testRef} />
+            <h1 ref={h1Ref}>{n}</h1>
             <button
                 onClick={() => {
-                    console.log(testRef);
-                    testRef.current.method();
+                    setN(n + 1)
                 }}
-            >点击调用Test组件的method方法</button>
+            >+</button>
         </div>
     )
 }
