c3286184d9c48dfd436449d29745476b054a7a34
zhichengyuan <1798788388@qq.com>
2019-12-24 15:26:49 +0800

    2-10完成

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,43 +1,20 @@
-import React,{ useState } from 'react'
-
-
-
-
-class Test extends React.PureComponent{
-    render() {
-        console.log('Test Render');
-        return <div>
-            <h1>{this.props.text}</h1>
-            <button onClick={this.props.onClick}>改变文本</button>
-        </div>
-    }
-}
-
-function Parent() {
-    console.log('Parent Render')
-    const [txt, setTxt] = useState(123);
-    const [n, setN] = useState(1)
-    return (
-            <div>
-                {/* 函数的地址每次渲染都发生了变化，导致了子组件跟着重新渲染，若子组件是经过优化组件，则导致优化失效*/}
-                <Test text={txt} onClick={() => {
-                    setTxt(Math.random());
-                }}/>
-                <input type="number"
-                value={n}
-                onChange={e => {
-                    setN(parseInt(e.target.value))
-                }}
-                />
-            </div>
-    )
-}
-
+import React,{useState,useRef} from 'react'
+window.arr = [];
 export default function App() {
-    console.log('App Render');
+    const inpRef = useRef();
+    window.arr.push(inpRef);
+    const [n, setN] = useState(0)
     return (
         <div>
-            <Parent/>
+            <input ref={inpRef} type="text"/>
+            <button onClick={() => {
+                console.log(inpRef.current.value);
+            }}>得到input的值</button>
+            <input type="number" value={n}
+                onChange={e => {
+                    setN(e.target.value);
+                }}
+            />
         </div>
     )
 }
