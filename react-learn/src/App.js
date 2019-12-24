1ffca27f2e7fc84feb62086999ede5e75d759a01
zhichengyuan <1798788388@qq.com>
2019-12-24 11:21:52 +0800

    2-9完成

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,44 +1,37 @@
-import React,{ useState,useCallback } from 'react'
+import React,{ useState,useMemo } from 'react'
 
-
-
-
-class Test extends React.PureComponent{
-    render() {
-        console.log('Test Render');
-        return <div>
-            <h1>{this.props.text}</h1>
-            <button onClick={this.props.onClick}>改变文本</button>
-        </div>
-    }
-}
-
-function Parent() {
-    console.log('Parent Render')
-    const [txt, setTxt] = useState(1);
-    const [n, setN] = useState(1);
-    const handleClick = useCallback(() => {
-        setTxt(txt + 1);
-    },[txt])
-    return (
-            <div>
-                {/* 函数的地址每次渲染都发生了变化，导致了子组件跟着重新渲染，若子组件是经过优化组件，则导致优化失效*/}
-                <Test text={txt} onClick={handleClick}/>
-                <input type="number"
-                value={n}
-                onChange={e => {
-                    setN(parseInt(e.target.value))
-                }}
-                />
-            </div>
-    )
+function Item(props) {
+    console.log('Item Render' + props.value)
+    return <li>{props.value}</li>
 }
 
 export default function App() {
-    console.log('App Render');
+    const [range,] = useState({min:1,max:10000});
+    const [n, setN] = useState(0)
+    const list = useMemo(() => {
+        const list = [];
+        for(let i = range.min;i<=range.max;i++) {
+            list.push(<Item key={i} value={i}></Item>)
+        }
+        return list;
+    },[range]);
+
+    // const list = [];
+    //     for(let i = range.min;i<=range.max;i++) {
+    //         list.push(<Item key={i} value={i}></Item>)
+    //     }
+    
     return (
         <div>
-            <Parent/>
+            <ul>
+                {list}
+            </ul>
+            <input type="number"
+            value={n}
+            onChange={e => {
+                setN(parseInt(e.target.value))
+            }}
+            />
         </div>
     )
 }
