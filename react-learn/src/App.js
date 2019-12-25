59c53d1afebb3e55244f78f698358da60ad5472d
zhichengyuan <1798788388@qq.com>
2019-12-25 15:39:22 +0800

    2-16完成

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,24 +1,39 @@
 import React, { useState } from 'react'
+import uuid from 'uuid'
 
-import { CSSTransition,SwitchTransition } from 'react-transition-group';
+import { CSSTransition,SwitchTransition,TransitionGroup } from 'react-transition-group';
 import './App.css'
 import 'animate.css'
 
 export default function App() {
-    const [show1, setShow1] = useState(true);
+    const [tasksList, setTasksList] = useState([
+        { id: uuid(),name:'任务1'},
+        { id:uuid(),name:'任务2' },
+    ]);
+    console.log(uuid())
     return (
-      <div className="container">
-            <SwitchTransition mode="out-in">
-                <CSSTransition appear timeout={800} key={show1}
-                    classNames={{
-                        exit:"bounceOut",
-                        enter:'bounceIn'
-                    }}
-                >
-                        <h1 className="animated fast">{show1?'title1':'title2'}</h1>
-                </CSSTransition>
-            </SwitchTransition>
-            <button onClick={() => setShow1(!show1)}>切换</button>
+      <div>
+            <TransitionGroup appear component='ul' className="dsd">
+                {
+                    tasksList.map(t => (
+                        <CSSTransition key={t.id} timeout={500}>
+                            <li>{t.name}<button
+                                onClick={() => {
+                                    var newTasks = tasksList.filter(it => it.id !== t.id)
+                                    setTasksList(newTasks);
+                                }}
+                            >删除</button></li>
+                        </CSSTransition>
+                    ))
+                }
+                
+            </TransitionGroup>
+            <button onClick={() => {
+                var name = window.prompt("请输入任务名称");
+                setTasksList([...tasksList,{id:uuid(),name}])
+            }}>
+                添加任务
+            </button>
       </div>
     );
   }
