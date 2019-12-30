2c4b40f53de01b64cffc03552174e1a12cf3c78e
zhichengyuan <1798788388@qq.com>
2019-12-30 16:56:39 +0800

    学生

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,42 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route,Switch } from 'react-router-dom'
-// /a
-function A() {
-    return <h1>组件A</h1>
-}
-// /b
-function B() {
-    return <h1>组件B</h1>
-}
-
-// /任意路径
-function C() {
-    return <h1>
-        找不到页面
-        <Route path="/abc" exact component={D}/>
-        </h1>
-}
-// /abc
-function D() {
-    return <p>组件D</p>
-}
+import Login from './pages/Login'
+import Admin from './pages/Admin'
 
 export default function App() {
     return(
-        <Router>
-            <Switch>
-                
-                <Route path="/a/b" component={B}/>
-                <Route path="/a" exact component={A}>
-                    {() => {
-                        return <div>
-                            <h1 style={{color:'red'}}>必定渲染</h1>
-                        </div>
-                    }}
-                </Route>
-                <Route component={C}/>
-            </Switch>
-        </Router>
-        
+      <Router>
+          <Switch>
+              <Route path="/login" exact component={Login}/>
+              <Route path="/" component={Admin}/>
+          </Switch>
+      </Router>
     )
 }
