f090745b001090623dcb37c73d8a4124e756ecf5
zhichengyuan <1798788388@qq.com>
2020-01-07 14:40:46 +0800

    sda

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router,Route} from 'react-router-dom'
 
+function Page1() {
+  return <h1>page1</h1>
+}
+function Page2() {
+  return <h1>page2</h1>
+}
 
 export default function App() {
     return(
