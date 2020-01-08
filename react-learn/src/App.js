1b32b54cb74c52ae5db0c726f569ca348e62d2b4
zhichengyuan <1798788388@qq.com>
2020-01-08 11:33:55 +0800

    fsf

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,38 +1,21 @@
 import React from 'react'
-import { Route,Link} from 'react-router-dom'
-import RouteGuard from './RouteGuard'
-
-function Page1() {
-  return <h1>page1</h1>
-}
-function Page2() {
-  return <h1>page2</h1>
-}
+import * as Pages from './page'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import './App.css'
+import TransitionRoute from './TransitionRoute'
 
 export default function App() {
     return(
-      <RouteGuard
-        onBeforeChange={(prev,cur,action,commit,unBlock) => {
-          console.log(`页面想要从${prev.pathname}跳转到${cur.pathname},跳转方式是${action},允许跳转`);
-          commit(true);
-          unBlock();//取消阻塞 ，只阻塞一次
-        }}
-        onChange={(precLocation,location,action,unListen) => {
-          console.log(`日志：从${precLocation.pathname}进入页面${location.pathname},进入方式${action}`)
-          unListen();//取消监听 ，只监听一次
-        }}
-      >
-       <ul>
-         <li>
-           <Link replace to="/page1">页面1</Link>
-         </li>
-         <li>
-           <Link to="/page2">页面2</Link>
-         </li>
-       </ul>
-          <Route path="/page1" component={Page1}/>
-          <Route path="/page2" component={Page2}/>
-      </RouteGuard>
+      <div className="main">
+        <Router>
+          <Pages.NavBar></Pages.NavBar>
+          <div className="page-container">
+            <TransitionRoute path="/" exact component={Pages.Home} />
+            <TransitionRoute path="/news" exact component={Pages.News}/>
+            <TransitionRoute path="/personal" exact component={Pages.Personal}/>
+          </div>
+        </Router>
+      </div>
     )
 }
 
