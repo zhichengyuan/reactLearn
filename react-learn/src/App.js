04be91ed6a034a063b31b5c762208eae310db8ff
zhichengyuan <1798788388@qq.com>
2020-01-14 12:05:29 +0800

    fds

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,33 +1,34 @@
 import React from 'react'
-import { BrowserRouter, Route,Switch } from "./react-router-dom"
+import { BrowserRouter, Route,withRouter } from "./react-router-dom"
+
+function Comp(props) {
+    return <div>
+        <button onClick={() => {
+            props.history.push("/page2")
+        }}>去page2</button>
+    </div>
+}
+
+const CompWithRouter = withRouter(Comp);
 
 function Page1() {
-    return <h1>Page1</h1>
+    return <div>
+        <h1>Page1</h1>
+        <CompWithRouter/>
+    </div>
 }
 
 function Page2() {
     return <h1>Page2</h1>
 }
 
-function Change({ history }) {
-    return <div>
-        <button onClick={() => {
-            history.push("/page1")
-        }}>去page1</button>
-        <button onClick={() => {
-            history.push("/page1/abc")
-        }}>去page2</button>
-    </div>
-}
+
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route path="/page1" component={Page1}/>
-                <Route path="/page1" component={Page1}/>
-                <Route path="/page1/abc" component={Page2} />
-            </Switch>
+            <Route path="/page1" component={Page1}/>
+            <Route path="/page2" component={Page2} />
         </BrowserRouter>
     )
 }
