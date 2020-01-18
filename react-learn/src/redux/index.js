be9f9c8c1dcf38449763ae9302d3e08dd6d29faa
zhichengyuan <1798788388@qq.com>
2020-01-18 13:24:42 +0800

    ddddddd

--- react-learn/src/redux/index.js
+++ react-learn/src/redux/index.js
@@ -1,9 +1,9 @@
-import { createStore,bindActionCreators } from 'redux'
+import { createStore} from 'redux'
 import reducer from './reducer'
 import * as numberActions from './action//number-action'
 
 
-const store = createStore(reducer,10);//创建store仓库
+const store = createStore(reducer);//创建store仓库
 
 console.log(store.getState());
 store.dispatch(numberActions.getIncreaseAction());
--- react-learn/src/redux/reducer/index.js
+++ react-learn/src/redux/reducer/index.js
@@ -4,17 +4,17 @@ import * as actionTypes from '../action/action-type'
  * @param state 之前仓库中的状态（数据）
  * @param action 描述要做什么的对象
  */
-export default function reducer(state,action) {
+export default function reducer(state = 10,action) {
     console.log('reducer运行了',state,action);
-    //返回一个新的状态
-    if(action.type === actionTypes.INCREASE) {
-        return state + 1;
+    switch (action.type) {
+        case actionTypes.INCREASE:
+            return state + 1;
+        case actionTypes.DECREASE:
+            return state - 1;
+        case actionTypes.SET:
+            return action.payload;
+        default:
+            return state;
     }
-    else if (action.type === actionTypes.DECREASE) {
-        return state -1
-    }
-    else if (action.type === actionTypes.SET) {
-        return action.payload;
-    }
-    return state;//如果是一个无效的操作类型，数据不变
+ 
 }
