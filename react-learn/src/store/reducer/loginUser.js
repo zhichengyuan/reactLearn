52c3d12b9725e9af27da0660c3c6d2595fe85aac
yuanzhicheng <1798788388@qq.com>
2020-02-03 23:30:59 +0800

    gggg

--- react-learn/src/store/reducer/loginUser.js
+++ react-learn/src/store/reducer/loginUser.js
@@ -3,6 +3,7 @@ import { SETLOGINUSERTYPE } from "../action/loginUserAction"
 const initialState = null;
 
 export default (state = initialState, { type, payload }) => {
+    
     switch (type) {
 
     case SETLOGINUSERTYPE:
