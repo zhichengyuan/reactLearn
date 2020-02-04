126a8de62d65e1be0e68ccd3556f2ad0f7e0f2cb
yuanzhicheng <1798788388@qq.com>
2020-02-04 11:31:37 +0800

    hhhhh

--- react-learn/src/store/index copy.js
+++ react-learn/src/store/index copy.js
@@ -1,4 +1,4 @@
-import { createStore,bindActionCreators} from '../redux'
+import { createStore,bindActionCreators} from 'redux'
 import reducer from './reducer'
 import { createAddUserAction,createDeleteUserAction } from './action/userAction'
 
