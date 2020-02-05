9c1ab0fa9f4ba05ab7189330cce53e4cdffa3757
yuanzhicheng <1798788388@qq.com>
2020-02-05 13:53:42 +0800

    ghff

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,43 +1,11 @@
 import { createStore,bindActionCreators, applyMiddleware} from '../redux'
 import reducer from './reducer'
 import { createAddUserAction,createDeleteUserAction } from './action/userAction'
+import logger from 'redux-logger'
 
-
-
-/**
- * 一个中间件函数
- * @param {*} store 
- */
- function logger1(store) {
-    return function (next) {
-        //下面返回的函数，是最终要应用的dispatch
-        return function (action) {
-            console.log('中间件1');
-            console.log('旧数据',store.getState());
-            console.log('action',action);
-            next(action);
-            console.log('新数据',store.getState());
-            console.log(" ")
-        }
-    }
- }
-
- function logger2(store) {
-    return function (next) {
-        //下面返回的函数，是最终要应用的dispatch
-        return function (action) {
-            console.log('中间件2');
-            console.log('旧数据',store.getState());
-            console.log('action',action);
-            next(action);
-            console.log('新数据',store.getState());
-            console.log(" ")
-        }
-    }
- }
 
 //应用中间件，方式1：
-const store = createStore(reducer,applyMiddleware(logger1,logger2));//创建store仓库
+const store = createStore(reducer,applyMiddleware(logger));//创建store仓库
 
 //方式2：
 // const store = applyMiddleware(logger1,logger2)(createStore)(reducer);
@@ -50,9 +18,7 @@ const actionCreators = {
 
 const actions = bindActionCreators(actionCreators,store.dispatch);
 
-store.subscribe(() => {
-    console.log('监听器',store.getState());
-})
+
 
 actions.addUsers({id:3,name:'sss',age:19});
 actions.deleteUser(3);
