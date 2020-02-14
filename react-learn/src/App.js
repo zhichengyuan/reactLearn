9d31fdcc7b7260c7a0c36f75097038ceb4d84f7f
yuanzhicheng <1798788388@qq.com>
2020-02-14 16:17:54 +0800

    dddss

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,36 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route,Link,NavLink} from "./react-router-dom"
-
-
-
-function Page1() {
-    return <div>
-        <h1>Page1</h1>
-    </div>
-}
-
-function Page2() {
-    return <h1>Page2</h1>
-}
-
-
+import {Provider} from 'react-redux'
+import store from './store'
+// import { BrowserRouter, Route,Link,NavLink} from "./react-router-dom"
+import Counter from './components/Counter'
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Route path="/page1" component={Page1}/>
-            <Route path="/page2" component={Page2} />
-            <ul>
-                <li>
-                    <Link to={{
-                        pathname:'/page1',
-                        search:'?a=1&b=2'
-                    }}>跳转到页面1</Link>
-                </li>
-                <li>
-                    <NavLink to="/page2">跳转到页面2</NavLink>
-                </li>
-            </ul>
-        </BrowserRouter>
+        <Provider store={store}>
+            <Counter abc={123}/>
+        </Provider>
+        
     )
+    // return <div>
+    //     <Counter/>
+    // </div>
 }
